970e95489b09d66373b85bdee7d82b194e2c59d5
OInVein <progthrash3305@gmail.com>
2023-04-03 00:18:59 +0800

    add form wrapper

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -7,20 +7,22 @@ type FormContainerProps = {
 
 function FormContainer({ children }: FormContainerProps) {
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      sx={{
-        flexDirection: {
-          xs: 'column',
-          md: 'row',
-        },
-      }}
-      p={2}
-      gap={2}
-    >
-      {children}
-    </Box>
+    <form>
+      <Box
+        display="flex"
+        justifyContent="center"
+        sx={{
+          flexDirection: {
+            xs: 'column',
+            md: 'row',
+          },
+        }}
+        p={2}
+        gap={2}
+      >
+        {children}
+      </Box>
+    </form>
   )
 }
 
