13ca6f5c7f266a49320ff9b7b88232b6e0b38146
OInVein <progthrash3305@gmail.com>
2023-04-02 22:11:18 +0800

    add react-router support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { theme } from './theme'
+import { PopulationSurveyPage } from './pages'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PopulationSurveyPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline enableColorScheme />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
