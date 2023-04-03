fb847f514fa137255e6e062163e0787e491fa339
OInVein <progthrash3305@gmail.com>
2023-04-03 22:43:13 +0800

    refactor with controller component for needless to set defaultValues manually

--- src/components/shared/Select.tsx
+++ src/components/shared/Select.tsx
@@ -7,46 +7,54 @@ import {
   useTheme,
   useMediaQuery,
 } from '@mui/material'
-import { useFormContext } from 'react-hook-form'
+import { Controller, useFormContext } from 'react-hook-form'
 import { SelectProps } from '../types'
 
 function Select({ id, label, placeholder, defaultValue, items, disabled = false }: SelectProps) {
   const theme = useTheme()
   const matches = useMediaQuery(theme.breakpoints.up('md'))
-  const { register } = useFormContext()
+  const { control } = useFormContext()
 
   return (
     <FormControl color="primary" size={matches ? 'small' : 'medium'}>
       <InputLabel shrink>{label}</InputLabel>
-      <MuiSelect
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...register(id)}
-        label={label}
-        disabled={disabled}
-        displayEmpty={Boolean(placeholder)}
+      <Controller
+        name={id}
+        control={control}
         defaultValue={defaultValue}
-        renderValue={(selected) => (!selected ? <em>{placeholder}</em> : (selected as ReactNode))}
-        sx={{
-          [theme.breakpoints.up('xs')]: {
-            height: theme.spacing(7),
-          },
-          [theme.breakpoints.up('md')]: {
-            minWidth: theme.spacing(24),
-            height: theme.spacing(5),
-          },
-        }}
-      >
-        {placeholder && (
-          <MenuItem disabled value="">
-            <em>{placeholder}</em>
-          </MenuItem>
+        render={({ field }) => (
+          <MuiSelect
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...field}
+            label={label}
+            disabled={disabled}
+            displayEmpty={Boolean(placeholder)}
+            renderValue={(selected) =>
+              !selected ? <em>{placeholder}</em> : (selected as ReactNode)
+            }
+            sx={{
+              [theme.breakpoints.up('xs')]: {
+                height: theme.spacing(7),
+              },
+              [theme.breakpoints.up('md')]: {
+                minWidth: theme.spacing(24),
+                height: theme.spacing(5),
+              },
+            }}
+          >
+            {placeholder && (
+              <MenuItem disabled value="">
+                <em>{placeholder}</em>
+              </MenuItem>
+            )}
+            {items.map(({ value, title }) => (
+              <MenuItem key={value} value={value}>
+                {title}
+              </MenuItem>
+            ))}
+          </MuiSelect>
         )}
-        {items.map(({ value, title }) => (
-          <MenuItem key={value} value={value}>
-            {title}
-          </MenuItem>
-        ))}
-      </MuiSelect>
+      />
     </FormControl>
   )
 }
