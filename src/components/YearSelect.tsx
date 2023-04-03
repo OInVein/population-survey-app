9dd69cb7c230c70eecf1ce54a3b2fa5c591a6de7
OInVein <progthrash3305@gmail.com>
2023-04-03 22:43:47 +0800

    refactor defaultvalues usage

--- src/components/YearSelect.tsx
+++ src/components/YearSelect.tsx
@@ -2,7 +2,7 @@ import { Select } from './shared'
 import { yearItems } from '../constants'
 
 function YearSelect() {
-  return <Select id="year" label="年份" defaultValue={yearItems[0].value} items={yearItems} />
+  return <Select id="year" label="年份" items={yearItems} />
 }
 
 export default YearSelect
