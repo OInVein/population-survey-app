5175eed8f99e6540d58cb425e405478b5d7edd8f
OInVein <progthrash3305@gmail.com>
2023-04-04 16:15:48 +0800

    add context to manage global state

---
+++ src/context/PopulationSurveyContext.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, createContext, useCallback, useContext, useState } from 'react'
+
+type PopulationSurveyContextType = {
+  theme?: 'dark' | 'light'
+  isLoading?: boolean
+  updateValue?: (newValue: PopulationSurveyContextType) => void
+}
+const initialState: PopulationSurveyContextType = {
+  theme: 'dark',
+  isLoading: false,
+  updateValue: () => {},
+}
+
+const PopulationSurveyContext = createContext<PopulationSurveyContextType>(initialState)
+const { Provider } = PopulationSurveyContext
+const usePopulationSurveyContext = () => useContext(PopulationSurveyContext)
+
+function PopulationSurveyProvider({ children }: { children: ReactNode }) {
+  const [value, setValue] = useState<PopulationSurveyContextType>(initialState)
+
+  const updateValue = useCallback((newValue: Omit<PopulationSurveyContextType, 'updateValue'>) => {
+    setValue((prev) => ({
+      ...prev,
+      ...newValue,
+    }))
+  }, [])
+
+  return <Provider value={{ ...value, updateValue }}>{children}</Provider>
+}
+
+export type { PopulationSurveyContextType }
+export { PopulationSurveyProvider, usePopulationSurveyContext }
