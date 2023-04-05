4390efab11f50bcec13e47bbfb268bece4502e7b
OInVein <progthrash3305@gmail.com>
2023-04-06 01:32:08 +0800

    refactor status of title

--- src/components/SearchResultTitle.tsx
+++ src/components/SearchResultTitle.tsx
@@ -23,7 +23,11 @@ function SearchResultTitle() {
         },
       })}
     >
-      {isLoading ? '搜尋中' : `搜尋結果${chartData && chartData.length === 0 ? ' - 查無資料' : ''}`}
+      {isLoading
+        ? '搜尋中'
+        : `${chartData ? '搜尋結果' : ''}${
+            chartData && chartData.length === 0 ? ' - 查無資料' : ''
+          }`}
     </Typography>
   )
 }
