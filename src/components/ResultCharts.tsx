486f8d07cedab83d265c0010cd4ee5358c5d7ff0
OInVein <progthrash3305@gmail.com>
2023-04-05 19:12:49 +0800

    add result borderRadius

--- src/components/ResultCharts.tsx
+++ src/components/ResultCharts.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, useTheme } from '@mui/material'
 import * as Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import { usePopulationSurveyContext } from '../context'
@@ -8,6 +8,7 @@ function ResultCharts() {
   const { chartData } = usePopulationSurveyContext()
   const populationOptions = usePopulationOptions()
   const hasData = chartData && chartData.length > 0
+  const theme = useTheme()
 
   return (
     <Box width={1} height={1} display="flex" justifyContent="center" alignItems="center">
@@ -15,7 +16,13 @@ function ResultCharts() {
         <HighchartsReact
           highcharts={Highcharts}
           options={populationOptions}
-          containerProps={{ style: { width: '100%', height: '100%' } }}
+          containerProps={{
+            style: {
+              width: '100%',
+              height: '100%',
+              borderRadius: theme.spacing(2),
+            },
+          }}
         />
       )}
     </Box>
