85a5c2d5eb3168c19220e2ab19750f5f43510d06
OInVein <progthrash3305@gmail.com>
2023-04-05 09:53:49 +0800

    implement mock hightcharts

--- src/components/ResultCharts.tsx
+++ src/components/ResultCharts.tsx
@@ -1,13 +1,31 @@
 import { Box, Typography } from '@mui/material'
+import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts'
 import { usePopulationSurveyContext } from '../context'
+
+const options = {
+  title: {
+    text: '人口數統計',
+  },
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+}
 
 function ResultCharts() {
   const { chartData } = usePopulationSurveyContext()
   const hasData = chartData && chartData.length > 0
+  console.log('chartData: ', chartData)
 
   return (
     <Box width={1} height={1} display="flex" justifyContent="center" alignItems="center">
-      {hasData ? <div>hasData</div> : <Typography variant="h1">👨‍🔬</Typography>}
+      {hasData ? (
+        <HighchartsReact highcharts={Highcharts} options={options} />
+      ) : (
+        <Typography variant="h1">👨‍🔬</Typography>
+      )}
     </Box>
   )
 }
