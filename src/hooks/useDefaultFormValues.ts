dadb608bc4e1e2cdf6fcc2ce45e5d27ff30c26b8
OInVein <progthrash3305@gmail.com>
2023-04-05 22:28:53 +0800

    refator useDefaultFormValues

--- src/hooks/useDefaultFormValues.ts
+++ src/hooks/useDefaultFormValues.ts
@@ -1,36 +1,14 @@
-import { useMemo } from 'react'
-import { useLocation } from 'react-router-dom'
 import { yearItems } from '../constants'
 import { FormValues } from '../components/types'
 import useValidateUrlParams from './useValidateUrlParams'
 
 const useDefaultFormValues = () => {
-  const { pathname } = useLocation()
   const { year, county, district } = useValidateUrlParams()
-  const defaultFormValues: FormValues = useMemo(() => {
-    const pathes = pathname.split('/').filter(Boolean)
-    const hasPathes = pathes.length === 3
-
-    return !hasPathes
-      ? {
-          year: yearItems[0].value,
-          county: '',
-          district: '',
-        }
-      : {
-          year: year.isValid ? year.value : yearItems[0].value,
-          county: county.isValid ? county.value : '',
-          district: district.isValid ? district.value : '',
-        }
-  }, [
-    pathname,
-    year.isValid,
-    year.value,
-    county.isValid,
-    county.value,
-    district.isValid,
-    district.value,
-  ])
+  const defaultFormValues: FormValues = {
+    year: year.isValid ? year.value : yearItems[0].value,
+    county: county.isValid ? county.value : '',
+    district: district.isValid ? district.value : '',
+  }
 
   return defaultFormValues
 }
