431516c0602d373608f0ce603a8ed3d3bb353aca
OInVein <progthrash3305@gmail.com>
2023-04-05 09:31:57 +0800

    define reponse type

--- src/services/population-servey.ts
+++ src/services/population-servey.ts
@@ -1,24 +1,42 @@
 import { FormValues } from '../components/types'
 
-// type FetchPupulationSurvey = (value: FormValues) => {
-//   district_code: string
-//   household_business_f: string
-//   household_business_m: string
-//   household_business_total: string
-//   household_ordinary_f: string
-//   household_ordinary_m: string
-//   household_ordinary_total: string
-//   household_single_f: string
-//   household_single_m: string
-//   household_single_total: string
-//   site_id: string
-//   statistic_yyy: string
-//   village: string
-// }
+type PopulationSurvey = {
+  district_code: string
+  household_business_f: string
+  household_business_m: string
+  household_business_total: string
+  household_ordinary_f: string
+  household_ordinary_m: string
+  household_ordinary_total: string
+  household_single_f: string
+  household_single_m: string
+  household_single_total: string
+  site_id: string
+  statistic_yyy: string
+  village: string
+}
 
-const fetchPopulationSurvey = ({ year, county, district }: FormValues) =>
-  fetch(
+type PopulationSurveyResponse = {
+  page: string
+  pageDataSize: string
+  responseCode: string
+  responseData: PopulationSurvey[]
+  responseMessage: string
+  totalDataSize: string
+  totalPage: string
+}
+
+const fetchPopulationSurvey = async ({
+  year,
+  county,
+  district,
+}: FormValues): Promise<PopulationSurvey[]> => {
+  const res = await fetch(
     `https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP019/${year}?COUNTY=${county}&TOWN=${district}`,
-  ).then((res) => res.json())
+  )
+  const { responseData } = <PopulationSurveyResponse>await res.json()
+
+  return responseData
+}
 
 export { fetchPopulationSurvey }
